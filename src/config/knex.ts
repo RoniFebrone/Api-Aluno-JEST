598326f0aaad8cf11d4e9f37111e1f0ded5b41e8
Roni Febrone de Almeida <132415797+RoniFebrone@users.noreply.github.com>
2024-05-21 07:02:40 -0300

    Update knex.ts

--- src/config/knex.ts
+++ src/config/knex.ts
@@ -3,26 +3,8 @@ export default {
   connection: {
     host: 'localhost',
     port: 3306,
-    user: 'roni',
-    password: 'RoniR613!',
+    user: 'user',
+    password: '',
     database: 'ApiAluno',
   },
 };
-
-
-// import Knex from 'knex';
-
-// const knexConfig = {
-//   client: 'mysql2',
-//   connection: {
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'roni',
-//     password: 'RoniR613!',
-//     database: 'ApiAluno',
-//   },
-// };
-
-// const knexInstance = Knex(knexConfig);
-
-// export default knexInstance;
